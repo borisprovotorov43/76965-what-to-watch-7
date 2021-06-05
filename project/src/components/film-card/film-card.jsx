25c2d0b5310ce6df9967bec47ef3152c82094b8e
Борис <boris@iMac-Boris.local>
2021-06-05 12:41:39 +0300

    Ошибки в img

--- project/src/components/film-card/film-card.jsx
+++ project/src/components/film-card/film-card.jsx
@@ -5,7 +5,7 @@ function FilmCard({image, title}) {
   return (
     <article className="small-film-card catalog__films-card">
       <div className="small-film-card__image">
-        <img src={image }alt={title} width="280" height="175" />
+        <img src={image} alt={title} width="280" height="175" />
       </div>
       <h3 className="small-film-card__title">
         <a className="small-film-card__link" href="film-page.html">{title}</a>
