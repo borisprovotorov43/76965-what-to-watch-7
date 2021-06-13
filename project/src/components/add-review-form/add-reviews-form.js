779aff63c7ff31a399d24f32d0053f1aea0046c5
Борис <boris@iMac-Boris.local>
2021-06-13 21:01:00 +0300

    Обратная сторона реальности - переименовал функции

--- project/src/components/add-review-form/add-reviews-form.js
+++ project/src/components/add-review-form/add-reviews-form.js
@@ -7,17 +7,19 @@ function AddReviewsForm() {
     reviewText: '',
   });
 
-  function ratingClickHandler(evt) {
+  function handleChangeRating(evt) {
+    const { value } = evt.target;
     setFormData({
       ...formData,
-      rating: evt.target.value,
+      rating: value,
     });
   }
 
-  function textareaInputHandler(evt) {
+  function handleChangeReviewText(evt) {
+    const { value } = evt.target;
     setFormData({
       ...formData,
-      reviewText: evt.target.value,
+      reviewText: value,
     });
   }
 
@@ -26,40 +28,40 @@ function AddReviewsForm() {
       <form action="#" className="add-review__form">
         <div className="rating">
           <div className="rating__stars">
-            <input className="rating__input" id="star-10" onChange={ratingClickHandler} type="radio" name="rating" value="10" />
+            <input className="rating__input" id="star-10" onChange={handleChangeRating} type="radio" name="rating" value="10" />
             <label className="rating__label" htmlFor="star-10">Rating 10</label>
 
-            <input className="rating__input" id="star-9" onChange={ratingClickHandler} type="radio" name="rating" value="9" />
+            <input className="rating__input" id="star-9" onChange={handleChangeRating} type="radio" name="rating" value="9" />
             <label className="rating__label" htmlFor="star-9">Rating 9</label>
 
-            <input className="rating__input" id="star-8" onChange={ratingClickHandler} type="radio" name="rating" value="8" checked />
+            <input className="rating__input" id="star-8" onChange={handleChangeRating} type="radio" name="rating" value="8" checked />
             <label className="rating__label" htmlFor="star-8">Rating 8</label>
 
-            <input className="rating__input" id="star-7" onChange={ratingClickHandler} type="radio" name="rating" value="7" />
+            <input className="rating__input" id="star-7" onChange={handleChangeRating} type="radio" name="rating" value="7" />
             <label className="rating__label" htmlFor="star-7">Rating 7</label>
 
-            <input className="rating__input" id="star-6" onChange={ratingClickHandler} type="radio" name="rating" value="6" />
+            <input className="rating__input" id="star-6" onChange={handleChangeRating} type="radio" name="rating" value="6" />
             <label className="rating__label" htmlFor="star-6">Rating 6</label>
 
-            <input className="rating__input" id="star-5" onChange={ratingClickHandler} type="radio" name="rating" value="5" />
+            <input className="rating__input" id="star-5" onChange={handleChangeRating} type="radio" name="rating" value="5" />
             <label className="rating__label" htmlFor="star-5">Rating 5</label>
 
-            <input className="rating__input" id="star-4" onChange={ratingClickHandler} type="radio" name="rating" value="4" />
+            <input className="rating__input" id="star-4" onChange={handleChangeRating} type="radio" name="rating" value="4" />
             <label className="rating__label" htmlFor="star-4">Rating 4</label>
 
-            <input className="rating__input" id="star-3" onChange={ratingClickHandler} type="radio" name="rating" value="3" />
+            <input className="rating__input" id="star-3" onChange={handleChangeRating} type="radio" name="rating" value="3" />
             <label className="rating__label" htmlFor="star-3">Rating 3</label>
 
-            <input className="rating__input" id="star-2" onChange={ratingClickHandler} type="radio" name="rating" value="2" />
+            <input className="rating__input" id="star-2" onChange={handleChangeRating} type="radio" name="rating" value="2" />
             <label className="rating__label" htmlFor="star-2">Rating 2</label>
 
-            <input className="rating__input" id="star-1" onChange={ratingClickHandler} type="radio" name="rating" value="1" />
+            <input className="rating__input" id="star-1" onChange={handleChangeRating} type="radio" name="rating" value="1" />
             <label className="rating__label" htmlFor="star-1">Rating 1</label>
           </div>
         </div>
 
         <div className="add-review__text">
-          <textarea className="add-review__textarea" name="review-text" id="review-text" onChange={textareaInputHandler} placeholder="Review text" />
+          <textarea className="add-review__textarea" name="review-text" id="review-text" onChange={handleChangeReviewText} placeholder="Review text" />
           <div className="add-review__submit">
             <button className="add-review__btn" type="submit">Post</button>
           </div>
